8036cfb6d22daec3eb17fb37a3144f5adba04ab0
officetracking <98247777+officetracking@users.noreply.github.com>
2022-02-11 13:16:21 +0700

    Add files via upload

---
+++ pages/assessment/create.tsx
@@ -0,0 +1,207 @@
+import {
+  Layout,
+  PageHeader,
+  Breadcrumb,
+  Button,
+  message,
+  Form,
+  Row,
+  Col,
+  Image,
+  Upload,
+} from "antd";
+import Head from "next/head";
+import PageLayout from "../../components/layouts/Layout";
+import { NextPage } from "next";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { FormInstance } from "rc-field-form";
+import _merge from "lodash/merge";
+import axios from "axios";
+import EmployeeForm from "../../components/employees/EmployeeForm";
+import AssessmentForm from "../../components/assessment/AssessmentForm";
+
+const { Content } = Layout;
+
+const AssessmentCreate: NextPage = () => {
+  const router = useRouter();
+  const [form] = Form.useForm();
+  const [isLoading, setIsLoading] = useState(false);
+  const [visible, setVisible] = useState(false);
+  const { Dragger } = Upload;
+
+  const onFormFinish = (values: any) => {
+    sendData(values);
+  };
+
+  const sendData = async (requestObject: any) => {
+    console.log(requestObject);
+    const hide = message.loading("Action in progress..", 0);
+    var data = requestObject.code.split(",");
+
+    setIsLoading(true);
+    await axios
+      .post("/api/assessment", {
+        code: data[0],
+        name: data[1],
+        day: requestObject.day,
+        total_order: requestObject.total_order,
+        result: requestObject.result,
+        persentase:
+          (requestObject.result / requestObject.total_order).toFixed(2) + "%",
+
+        method: "POST",
+      })
+      .then((res) => {
+        hide();
+        router.push("/assessment");
+      })
+      .catch((err) => {
+        hide();
+        console.log(err.response);
+        const { data } = err.response;
+        message.error(data.message);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
+
+  const onFormFinishFailed = (content: string) => {
+    message.error(content);
+  };
+
+  const onSubmitForm = (form: FormInstance) => {
+    form.submit();
+  };
+
+  return (
+    <>
+      <Head>
+        <title>Monitoring - Tambah Employee</title>
+        <meta name="description" content="Monitoring Teknisi Lapangan" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <PageLayout>
+        <div style={{ padding: "0 24px", marginTop: 64 + 24 }}>
+          <Breadcrumb>
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+            <Breadcrumb.Item>
+              <Link href="/outlets">
+                <a>Assessment</a>
+              </Link>
+            </Breadcrumb.Item>
+            <Breadcrumb.Item>Tambah</Breadcrumb.Item>
+          </Breadcrumb>
+          <PageHeader
+            className="site-page-header"
+            title="Assessment"
+            // breadcrumb={{ routes }}
+            subTitle="Kelola Penilaian"
+            onBack={() => router.push("/outlets")}
+            style={{
+              // borderBottom: "1px solid rgb(235, 237, 240)",
+              // marginBottom: 24,
+              padding: 0,
+              paddingBottom: 16,
+            }}
+          />
+          <Content className="site-layout">
+            <div
+              className="site-layout-background"
+              style={{
+                padding: 24,
+                minHeight: "calc(100vh - (24px + 24px + 24px + 64px))",
+              }}
+            >
+              {/* <Row justify="center" style={{ padding: "10px 0" }}>
+                <Col span={16} xl={16} md={24}>
+                  <Steps current={current}>
+                    {steps.map((item: Step) => (
+                      <Step
+                        key={item.title}
+                        title={item.title}
+                        description={item.description}
+                      />
+                    ))}
+                  </Steps>
+                </Col>
+              </Row> */}
+              <div className="steps-content" style={{ marginTop: 50 }}>
+                <Row justify="start">
+                  <Col
+                    span={16}
+                    xl={16}
+                    md={24}
+                    // style={{ border: "1px solid #f0f0f0", padding: 40 }}
+                  >
+                    <AssessmentForm
+                      form={form}
+                      onFinish={onFormFinish}
+                      onFinishFailed={() =>
+                        onFormFinishFailed("Isi Form dengan sesuai")
+                      }
+                    />
+                    {/* {current == 0 && (
+                      <InformationForm
+                        form={informationForm}
+                        onFinish={onInformationFormFinish}
+                        onFinishFailed={() =>
+                          onFormFinishFailed("Isi Form dengan sesuai")
+                        }
+                      />
+                    )}
+                    {current == 1 && (
+                      <GoodsForm
+                        form={goodsForm}
+                        onFinish={onGoodsFormFinish}
+                        onFinishFailed={() =>
+                          onFormFinishFailed("Isi Form dengan sesuai")
+                        }
+                      />
+                    )}
+                    {current == 2 && (
+                      <LocationForm
+                        form={locationForm}
+                        onFinish={onLocationFormFinish}
+                        onFinishFailed={() =>
+                          onFormFinishFailed("Isi Form dengan sesuai")
+                        }
+                        current={current}
+                        lat={lat}
+                        lng={lng}
+                        zoom={zoom}
+                        setLatLng={setLatLng}
+                        onChangeLat={setLat}
+                        onChangeLng={setLng}
+                      />
+                    )} */}
+                  </Col>
+                </Row>
+              </div>
+              <div className="action" style={{ marginTop: 20 }}>
+                <Row justify="start">
+                  <Col>
+                    <Button
+                      style={{ margin: "0 8px" }}
+                      type="primary"
+                      onClick={() => {
+                        onSubmitForm(form);
+                      }}
+                      disabled={isLoading}
+                    >
+                      Simpan
+                    </Button>
+                  </Col>
+                </Row>
+              </div>
+            </div>
+          </Content>
+        </div>
+      </PageLayout>
+    </>
+  );
+};
+
+export default AssessmentCreate;
---
+++ pages/assessment/index.js
@@ -0,0 +1,230 @@
+import {
+  Layout,
+  PageHeader,
+  Table,
+  Tag,
+  Space,
+  Avatar,
+  Button,
+  Tooltip,
+  Row,
+  Col,
+  Card,
+  Statistic,
+  Breadcrumb,
+  Popconfirm,
+  message,
+} from "antd";
+import Head from "next/head";
+
+// import styles from "../styles/Home.module.css";
+import {
+  UserOutlined,
+  PlusOutlined,
+  DownloadOutlined,
+  DeleteOutlined,
+  EditOutlined,
+  ArrowDownOutlined,
+  ArrowUpOutlined,
+} from "@ant-design/icons";
+import PageLayout from "../../components/layouts/Layout";
+import { NextPage } from "next";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import swal from "sweetalert";
+import ButtonGroup from "antd/lib/button/button-group";
+import jsPDF from "jspdf";
+
+const { Content } = Layout;
+require("jspdf-autotable");
+
+const routes = [
+  {
+    path: "index",
+    breadcrumbName: "Home",
+  },
+  {
+    path: "first",
+    breadcrumbName: "Pegawai",
+  },
+  {
+    path: "second",
+    breadcrumbName: "Kelola",
+  },
+];
+
+const AssessmentIndex = () => {
+  const router = useRouter();
+  const [data, setData] = useState([]);
+
+  const columns = [
+    {
+      title: "NIK",
+      dataIndex: "code",
+      key: "code",
+      render: (text) => <a>{text}</a>,
+    },
+    {
+      title: "Nama",
+      dataIndex: "name",
+      key: "name",
+      render: (text) => <a>{text}</a>,
+    },
+    {
+      title: "Jumlah Orderan",
+      dataIndex: "total_order",
+      key: "total_order",
+    },
+    {
+      title: "Hasil Progress",
+      dataIndex: "result",
+      key: "result",
+    },
+
+    {
+      title: "Presentasi hasil",
+      dataIndex: "persentase",
+      key: "persentase",
+    },
+  ];
+
+  function onDelete(code) {
+    console.log(code);
+    swal({
+      title: "Are you sure?",
+      text: "Data Akan Dihapus",
+      icon: "warning",
+
+      dangerMode: true,
+    }).then((willDelete) => {
+      if (willDelete) {
+        axios
+          .delete(`/api/employees/${code}`)
+          .then((response) => {
+            fetchEmployees();
+            swal("Data di hapus", {
+              icon: "success",
+            });
+          })
+          .catch((err) => {
+            console.log(err);
+            swal("Data tidak berhasil dihapus", {
+              icon: "error",
+            });
+          });
+      } else {
+      }
+    });
+  }
+
+  const fetchEmployees = async () => {
+    let response = await axios.get("/api/assessment");
+    setData(response.data.entriesData);
+    console.log("data assessment", response.data.entriesData);
+  };
+
+  useEffect(() => {
+    fetchEmployees();
+  }, []);
+
+  const report = async () => {
+    //await checkinHistory(employeeCode);
+    var pdf = [];
+    var doc = new jsPDF("p", "px", "a4");
+    doc.setFontSize(15);
+    doc.text(`Perhitungan Evaluasi harian  `, 10, 20);
+
+    doc.autoTable({ html: "#my-table", margin: { top: 20 } });
+    data.map((value) => {
+      var data = [
+        // dateFormat(value.date, "dd/mm/yyyy"),
+        `${value.code}`,
+        `${value.name}`,
+        `${value.total_order}`,
+        `${value.result}`,
+        `${value.persentase}`,
+      ];
+      pdf.push(data);
+    });
+
+    doc.autoTable({
+      margin: { top: 50 },
+      headStyles: {
+        fillColor: "#3498db",
+        textColor: [255, 255, 255],
+        fontSize: 10,
+        padding: 0,
+      },
+
+      thema: "grid",
+      margin: { left: 10, right: 10, top: 100 },
+      head: [["NIK", "Nama", "Jumlah Order", "Hasil Progress", "Persentase"]],
+      body: pdf,
+    });
+    window.open(doc.output("bloburl"), "_blank");
+  };
+  return (
+    <>
+      <Head>
+        <title>Monitoring - Employee</title>
+        <meta name="description" content="Monitoring Teknisi Lapangan" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <PageLayout>
+        <div style={{ padding: "0 24px", marginTop: 64 + 24 }}>
+          <Breadcrumb>
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+            <Breadcrumb.Item>Assessment Employee</Breadcrumb.Item>
+          </Breadcrumb>
+          <PageHeader
+            className="site-page-header"
+            title="Assessment Employee"
+            subTitle="Kelola  Penilaian"
+            style={{
+              padding: 0,
+              paddingBottom: 16,
+            }}
+          />
+          <Content className="site-layout">
+            <div
+              className="site-layout-background"
+              style={{
+                padding: 24,
+                minHeight: "calc(100vh - (24px + 24px + 24px + 64px))",
+              }}
+            >
+              <div style={{ marginBottom: 24, textAlign: "right" }}>
+                <Space>
+                  <Button
+                    onClick={() => {
+                      // console.log("tes");
+                      report();
+                    }}
+                    icon={<DownloadOutlined />}
+                  >
+                    Report .pdf
+                  </Button>
+                  <Button
+                    type="primary"
+                    onClick={() => router.push("/assessment/create")}
+                    icon={<PlusOutlined />}
+                  >
+                    {/* <Link href="/outlets/create">
+                      <a>Outlet Baru</a>
+                    </Link> */}
+                    Tambahkan
+                  </Button>
+                </Space>
+              </div>
+              <Table columns={columns} dataSource={data} />
+            </div>
+          </Content>
+        </div>
+      </PageLayout>
+    </>
+  );
+};
+
+export default AssessmentIndex;
